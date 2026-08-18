da8e640136d80676e0699c89533d8389d32f9de5
KiranRajeev-KV <kiranrajeevkv@gmail.com>
2026-08-18 20:05:01 +0530

    chore: remove obsolete private idea route

--- src/routes/_authenticated/ideas/$ideaId/index.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from "@tanstack/react-router";
-
-import { IdeaDetail } from "../../../../features/ideas/idea-detail.js";
-
-export const Route = createFileRoute("/_authenticated/ideas/$ideaId/")({
-  component: IdeaDetail,
-});
